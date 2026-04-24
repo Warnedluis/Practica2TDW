503fc0497273a095b295df016814aca757ea9c6f
navarrovegajosedavid23 <navarrovegajosedavid@gmail.com>
2026-04-23 20:00:55 -0600

    arreglaado

--- Practica2/src/main/java/com/upiiz/Practica2/controllers/HomeController.java
+++ Practica2/src/main/java/com/upiiz/Practica2/controllers/HomeController.java
@@ -1,18 +1,30 @@
 package com.upiiz.Practica2.controllers;
 
+// Asegúrate de que estos imports coincidan con tus carpetas
+import com.upiiz.Practica2.services.ServiciosLuis.AutorServiceImpl;
+import com.upiiz.Practica2.services.ServiciosLuis.LibroServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/home")
-public class HomeController{
+public class HomeController {
 
+    @Autowired
+    AutorServiceImpl autorService;
+
+    @Autowired
+    LibroServiceImpl libroService;
 
     @GetMapping("/MostrarInicio")
-    public String mostrarInicio() {
+    public String mostrarInicio(Model model) {
+        // AQUÍ ESTÁ LA MAGIA: Le pasamos las listas al HTML
+        model.addAttribute("listaAutores", autorService.listarAutores());
+        model.addAttribute("listaLibros", libroService.listarLibros());
+
         return "home";
     }
-
 }
